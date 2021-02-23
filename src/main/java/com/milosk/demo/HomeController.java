dbe0f45e32bc010f8b9d9bae728882cdbb14fb44
unknown <miloss_kovac@yahoo.com>
2021-02-23 19:01:54 +0100

    Prvi commit

---
+++ src/main/java/com/milosk/demo/HomeController.java
@@ -0,0 +1,33 @@
+package com.milosk.demo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+/*	@RequestMapping("home")
+	public String home(HttpServletRequest req) {
+		
+		HttpSession session = req.getSession();
+		String name = req.getParameter("name");
+		System.out.println("Name is " + name);
+		session.setAttribute("name", name);
+		return "home"; */
+	@RequestMapping("home")
+	public ModelAndView home(@RequestParam("name") String myName) {
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name", myName);
+		mv.setViewName("home");
+		return mv;
+	
+	}
+
+}
+
---
+++ src/main/java/com/milosk/demo/MyWebDemoApplication.java
@@ -0,0 +1,13 @@
+package com.milosk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebDemoApplication.class, args);
+	}
+
+}
